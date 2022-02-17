c020d06d20fa6380185e98e88953c978340b926e
Serhii <Tsenzuk@users.noreply.github.com>
2022-02-17 22:01:41 +0200

    add steps counter

---
+++ js/index.js
@@ -0,0 +1,48 @@
+function showWelcomeMessageAndAddSteps () {
+    addStep();
+    var steps = getSteps();
+
+    if (steps !== 0) {
+        document.getElementsByClassName('welcome-message').innerHTML = 'You wander here already for ' + steps + ' steps.';
+    }
+}
+
+function showGoodbyMessageAndResetSteps () {
+    var steps = getSteps();
+
+    if (steps !== 0) {
+        document.getElementsByClassName('goodby-message').innerHTML = 'You wandered here for ' + steps + ' steps.';
+    }
+
+    resetSteps();
+}
+
+// sesstion storage used, as it clears when page is closed
+function addStep() {
+    // get steps amount from session storage
+    var steps = window.sessionStorage.getItem('steps');
+
+    if (!steps) {
+        // if it is first function call, steps are empty. Set them to default value 
+        steps = 0;
+    } else {
+        // session storage keeps everything as strings, so to iterate we need to convert it to number
+        steps = parseInt(steps, 10);
+    
+        // iterate
+        steps = steps + 1;
+    }
+
+    // save steps amount
+    window.sessionStorage.setItem('steps', steps);
+}
+
+function resetSteps() {
+    window.sessionStorage.setItem('steps', 0);
+}
+
+function getSteps() {
+    var steps = window.sessionStorage.getItem('steps');
+
+    return steps;
+}
